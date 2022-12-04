c5c2f83986b355fb831f2fb4cca7aa02bfcd120f
Kristian Skogberg <k.skogberg98@gmail.com>
2022-12-04 14:31:46 +0200

    Scroll on top on page change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Navbar from "./components/Navbar";
-import { Route, Routes } from "react-router-dom";
+import {
+  Route,
+  Routes,
+  ScrollRestoration,
+  BrowserRouter,
+} from "react-router-dom";
 import Home from "./components/pages/Home";
 import Moves from "./components/pages/Moves";
 import Abilities from "./components/pages/Abilities";
 import Pokemon from "./components/pages/Pokemon";
 import Types from "./components/pages/Types";
 import Move from "./components/pages/Move";
+import ScrollToTop from "./utils/ScrollToTop";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Footer from "./components/Footer";
@@ -21,17 +27,20 @@ function App() {
   return (
     <>
       <QueryClientProvider client={client}>
-        <Navbar />
-        <div className="pt-[50px]">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/pokemon/:pokemon" element={<Pokemon />} />
-            <Route path="/move/" element={<Moves />} />
-            <Route path="/move/:move" element={<Move />} />
-            <Route path="/ability/:ability" element={<Abilities />} />
-            <Route path="/type/:type" element={<Types />} />
-          </Routes>
-        </div>
+        <BrowserRouter>
+          <ScrollToTop />
+          <Navbar />
+          <div className="pt-[50px]">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/pokemon/:pokemon" element={<Pokemon />} />
+              <Route path="/move/" element={<Moves />} />
+              <Route path="/move/:move" element={<Move />} />
+              <Route path="/ability/:ability" element={<Abilities />} />
+              <Route path="/type/:type" element={<Types />} />{" "}
+            </Routes>{" "}
+          </div>{" "}
+        </BrowserRouter>
         <Footer />
       </QueryClientProvider>
     </>
--- src/index.js
+++ src/index.js
@@ -5,14 +5,13 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import store from "./redux/store";
 import { Provider } from "react-redux";
+import ScrollToTop from "./utils/ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
